daac97f3af21e79ff046d98132b882ddbf0accbd
Adrian290404 <adrianmg2004.29@gmail.com>
2025-03-24 21:20:57 +0100

    autoclicker

---
+++ otro/autoclicker.py
@@ -0,0 +1,14 @@
+import pyautogui
+import time
+import keyboard
+
+print("Autoclicker iniciado. Presiona SPACE para detenerlo.")
+
+while True:
+    if keyboard.is_pressed('space'):
+        print("Autoclicker detenido.")
+        break
+    screen_width, screen_height = pyautogui.size()
+    center_x, center_y = screen_width // 2, screen_height // 2
+    pyautogui.click(center_x, center_y)
+    time.sleep(5)
